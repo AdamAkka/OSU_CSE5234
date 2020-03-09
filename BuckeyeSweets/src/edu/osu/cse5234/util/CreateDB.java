0ad339cab5389ff4e58643a65a3858fc35df63df
212600883 <212600883@GDTVW0Z2E.logon.ds.ge.com>
2020-03-08 21:21:43 -0400

    Lab 8

---
+++ BuckeyeSweets/src/edu/osu/cse5234/util/CreateDB.java
@@ -0,0 +1,13 @@
+package edu.osu.cse5234.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class CreateDB {
+	
+		public static void main(String[] args) throws Exception {
+			
+			Class.forName("org.h2.Driver").newInstance();
+			Connection conn = DriverManager.getConnection("jdbc:h2:" + "C:/Users/212600883/git/OSU_CSE5234/h2db/BuckeyeSweetsDB", "sa", "" );
+			System.out.println("Database has been created!");
+		}
+}
